b4c31fb8dccc1a2d6f58232b4a23c42b2c0fbaf5
yimiaoxiehou <yimiaoxiehou@gmail.com>
2025-03-20 11:05:10 +0800

    add pre.sh

--- pre.sh
+++ pre.sh
@@ -1,1 +1,2 @@
-wget  "https://github.com/tianon/gosu/releases/download/1.17/gosu-ppc64el
+wget "https://github.com/tianon/gosu/releases/download/1.17/gosu-ppc64el
+wget  https://github.com/thkukuk/rpcsvc-proto/releases/download/v1.4/rpcsvc-proto-1.4.tar.gz
